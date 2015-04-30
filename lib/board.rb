12e457ee4b93c9252a488ce6de72df31b2da57a5
Ian MacDonald & Kim Dang <student@epicodus.com>
2015-04-30 16:41:25 -0700

    refactor board specs.

--- lib/board.rb
+++ lib/board.rb
@@ -3,7 +3,6 @@ require('pry')
 class Board
 
   define_method(:initialize) do
-    # @spaces = []
     y = 1
     x = 1
     until y == 4
@@ -18,24 +17,7 @@ class Board
     @id
   end
 
-  define_method(:spaces) do
-    @spaces
-    # @spaces_array = []
-    # @spaces.each() do |space|
-    #   @space_array = []
-    #   @space_array.push(space.x().to_i())
-    #   @space_array.push(space.y().to_i())
-    #   @spaces_array.push(@space_array)
-    # end
-    # @spaces_array
-  end
-
   define_method(:find) do |id|
     Space.find(id)
-    # @spaces[index]
-    # @coordinates = []
-    # @coordinates.push(@spaces[index].x())
-    # @coordinates.push(@spaces[index].y())
-    # @coordinates
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,13 +2,6 @@ require('rspec')
 require('board')
 
 describe(Board) do
-  describe('#create') do
-    it('creates 9 spaces when the board is initialized') do
-      test_board = Board.new()
-      expect(test_board.spaces()).to(eq(test_board.spaces()))
-    end
-  end
-
   describe('#find') do
     it('finds and returns a specific space on the board') do
       test_board = Board.new()
